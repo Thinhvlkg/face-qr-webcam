4b92d39ea1ca7af4c5e304156faf9ecb66a86b4e
Thinhvlkg <140480144+Thinhvlkg@users.noreply.github.com>
2023-08-10 22:05:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ def ham_diem_danh():
     FILE_ID = "1h3u6qf-pqsfDuYNWqDM2GOxquCQA_Des"
     file_ds_lop = "DANH SACH NHAN CHINH THUC_1.xlsx"
     # Load facemodel.pkl
-    facenet_model = load_model("20180402-114759.pb")
+    facenet_model = load_model("Models/20180402-114759.pb")
     model = None
     class_names = None
     with open(CLASSIFIER_PATH, 'rb') as file:
