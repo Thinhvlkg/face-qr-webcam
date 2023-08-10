52992c3ca3b45c6ffd36a4ca5dda42a2af4a87db
Thinhvlkg <140480144+Thinhvlkg@users.noreply.github.com>
2023-08-10 22:16:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ def ham_diem_danh():
     FILE_ID = "1h3u6qf-pqsfDuYNWqDM2GOxquCQA_Des"
     file_ds_lop = "DANH SACH NHAN CHINH THUC_1.xlsx"
     # Load facemodel.pkl
-    facenet_model = load_model("Models/20180402-114759.pb")
+    facenet_model = load_model("20180402-114759.pb")
     model = None
     class_names = None
     with open(CLASSIFIER_PATH, 'rb') as file:
@@ -148,7 +148,7 @@ def ham_diem_danh():
     print("Custom Classifier, Successfully loaded")    
     # Load Facenet model from Google Drive
     gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", "20180402-114759.pb")
-    FACENET_MODEL_PATH = "Models/20180402-114759.pb"
+    FACENET_MODEL_PATH = "20180402-114759.pb"
     wb = openpyxl.load_workbook(file_ds_lop)
     sheet = wb.active
     with open(CLASSIFIER_PATH, 'rb') as file:
